19259ab97d68adb045d8e4277dfbe2261bced0d4
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-03-28 12:20:05 +0300

    layout of the view controller

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet private var paletteView: UIView!
+    
+    @IBOutlet private var redValue: UILabel!
+    @IBOutlet private var greenValue: UILabel!
+    @IBOutlet private var blueValue: UILabel!
+    
+    @IBOutlet private var redSlider: UISlider!
+    @IBOutlet private var greenSlider: UISlider!
+    @IBOutlet private var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        paletteView.layer.cornerRadius = 15
+        setColor()
+        
+        redValue.text = string(from: redSlider)
+        greenValue.text = string(from: greenSlider)
+        blueValue.text = string(from: blueSlider)
     }
-
-
+    
+    @IBAction private func SliderAction(_ sender: UISlider) {
+        setColor()
+        switch sender {
+            case redSlider:
+                redValue.text = string(from: redSlider)
+            case greenSlider:
+                greenValue.text = string(from: greenSlider)
+            default:
+                blueValue.text = string(from: blueSlider)
+        }
+    }
+    
+    private func setColor() {
+        paletteView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value) ,
+            alpha: 1
+        )
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
 }
-
